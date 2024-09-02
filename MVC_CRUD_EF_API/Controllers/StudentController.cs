5e93a6ff73fad906c685a788b19073b50bbef7fb
csant26 <shishantstha@gmail.com>
2024-09-02 23:16:31 +0545

    commit

---
+++ MVC_CRUD_EF_API/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_CRUD_EF_API.Models;
+using Newtonsoft.Json;
+
+namespace MVC_CRUD_EF_API.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string url = "https://localhost:7070/api/StudentAPI/";
+        private HttpClient client = new HttpClient();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>();
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert
+            }
+        }
+    }
+}
--- MVC_CRUD_EF_API/Models/Student.cs
+++ MVC_CRUD_EF_API/Models/Student.cs
@@ -1,6 +1,13 @@
 ﻿namespace MVC_CRUD_EF_API.Models
 {
+
     public class Student
     {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string gender { get; set; }
+        public string standard { get; set; }
+        public int age { get; set; }
     }
+
 }
