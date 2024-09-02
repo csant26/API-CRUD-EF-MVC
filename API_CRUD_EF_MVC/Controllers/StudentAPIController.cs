a5988045391a670f0f14cea458c77ee60b612359
csant26 <shishantstha@gmail.com>
2024-09-02 17:19:01 +0545

    first

---
+++ API_CRUD_EF_MVC/Controllers/StudentAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_CRUD_EF_MVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+    }
+}
